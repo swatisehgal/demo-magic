644ffa3309c08db21b5384750f1fb6ad28263c8c
Swati Sehgal <swsehgal@redhat.com>
2021-03-12 17:23:52 +0000

    PPC for sprint demo
    Signed-off-by: Swati Sehgal <swsehgal@redhat.com>

--- samples/demo-template.sh
+++ samples/demo-template.sh
@@ -20,23 +20,27 @@
 #
 # see http://www.tldp.org/HOWTO/Bash-Prompt-HOWTO/bash-prompt-escape-sequences.html for escape sequences
 #
-DEMO_PROMPT="${GREEN}➜ ${CYAN}\W "
+# DEMO_PROMPT="${GREEN}➜ ${CYAN}\W "
 
 # hide the evidence
-clear
+# clear
 
 
 # put your demo awesomeness here
-if [ ! -d "stuff" ]; then
-  pe "mkdir stuff"
-fi
-
-pe "cd stuff"
-
-pe "ls"
-
-p "cat \"something you dont want to really run\""
-
-# show a prompt so as not to reveal our true nature after
-# the demo has concluded
-p ""
+pe "cd /home/swsehgal/go/src/github.com/openshift-kni/performance-addon-operators"
+p "# Compiling the Performance Profile Creator"
+pe "make create-performance-profile"
+p "# Running the Performance Profile Creator to see the possible command line arguments available"
+pe "./build/_output/bin/performance-profile-creator -h"
+PROMPT_TIMEOUT=5
+wait
+p "# Running the Performance Profile Creator to create a profile"
+pe "./build/_output/bin/performance-profile-creator --mcp-name=worker-cnf --reserved-cpu-count=20 --rt-kernel=true --split-reserved-cpus-across-numa=false --topology-manager-policy=single-numa-node --must-gather-dir-path testdata/must-gather/must-gather.local.directory --power-consumption-mode=ultra-low-latency > performace-profile.yaml"
+PROMPT_TIMEOUT=5
+wait
+pe "cat performace-profile.yaml"
+PROMPT_TIMEOUT=5
+wait
+p "# Running the Performance Profile Creator using wrapper script"
+pe "./hack/run-perf-profile-creator.sh -t must-gather.tar.gz -- --mcp-name=worker-cnf --reserved-cpu-count=20 --rt-kernel=false --split-reserved-cpus-across-numa=true --topology-manager-policy=restricted  --power-consumption-mode=low-latency > performace-profile.yaml"
+pe "cat performace-profile.yaml"
